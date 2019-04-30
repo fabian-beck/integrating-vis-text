be1887711f48fe389e80118467fba2a2ebb7fb19
Fabian Beck <fabian@fbeck.com>
2019-04-30 18:52:40 +0200

    Improved title layout and added header

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 let titleContainer = document.createElement('div');
-titleContainer.classList.add('titleContainer');
-document.body.prepend(titleContainer);
+titleContainer.id = 'titleContainer';
 
 let titleDiv = document.createElement('div');
-titleDiv.classList.add('title');
+titleDiv.id = 'title';
 titleDiv.innerHTML = document.title;
 titleContainer.append(titleDiv);
 
 let authorDiv = document.createElement('div');
-authorDiv.classList.add('author');
+authorDiv.id = 'author';
 authorDiv.innerHTML = document.head.querySelector('[name~=author][content]').content;
 titleContainer.append(authorDiv);
+
+let headerDiv = document.createElement('div');
+headerDiv.id = 'header';
+
+document.body.prepend(headerDiv);
+document.body.prepend(titleContainer);
+
+window.onscroll = function() {updateStickyHeader()};
+
+let headerStickyOffset = header.offsetTop;
+function updateStickyHeader() {
+    if (window.pageYOffset > headerStickyOffset) {
+        header.classList.add('sticky');
+        header.innerHTML = document.title;
+    } else {
+        header.classList.remove('sticky');
+        header.innerHTML = '';
+    }
+}

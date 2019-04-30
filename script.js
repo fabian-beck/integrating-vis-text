ab48988dbb4f7caf5bd5fc70dfb4cc80efe90c4b
Fabian Beck <fabian@fbeck.com>
2019-04-30 19:21:59 +0200

    Refactored js code

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-let titleContainer = document.createElement('div');
-titleContainer.id = 'titleContainer';
-
-let titleDiv = document.createElement('div');
-titleDiv.id = 'title';
-titleDiv.innerHTML = document.title;
-titleContainer.append(titleDiv);
-
-let authorDiv = document.createElement('div');
-authorDiv.id = 'author';
-authorDiv.innerHTML = document.head.querySelector('[name~=author][content]').content;
-titleContainer.append(authorDiv);
-
-let headerDiv = document.createElement('div');
-headerDiv.id = 'header';
-
-document.body.prepend(headerDiv);
-document.body.prepend(titleContainer);
-
-window.onscroll = function() {updateStickyHeader()};
-
-let headerStickyOffset = header.offsetTop;
-function updateStickyHeader() {
-    if (window.pageYOffset > headerStickyOffset) {
-        header.classList.add('sticky');
-        header.innerHTML = document.title;
-    } else {
-        header.classList.remove('sticky');
-        header.innerHTML = '';
+let titleObject = {
+    create() {
+        let titleContainer = document.createElement('div');
+        titleContainer.id = 'titleContainer';
+        let titleDiv = document.createElement('div');
+        titleDiv.id = 'title';
+        titleDiv.innerHTML = document.title;
+        titleContainer.append(titleDiv);
+        let authorDiv = document.createElement('div');
+        authorDiv.id = 'author';
+        authorDiv.innerHTML = document.head.querySelector('[name~=author][content]').content;
+        titleContainer.append(authorDiv);
+        return titleContainer;
     }
 }
+
+let headerObject = {
+    stickyOffset: 0,
+    create() {
+        let headerDiv = document.createElement('div');
+        headerDiv.id = 'header';
+        return headerDiv;
+    },
+    /* needs to be called only after the whole layout has been established */
+    initStickyHeader() {
+        this.stickyOffset = header.offsetTop;
+        window.onscroll = function () { headerObject.updateStickyHeader() };
+    },
+    updateStickyHeader() {
+        if (window.pageYOffset > this.stickyOffset) {
+            header.classList.add('sticky');
+            header.innerHTML = document.title;
+        } else {
+            header.classList.remove('sticky');
+            header.innerHTML = '';
+        }
+    }
+}
+
+document.body.prepend(headerObject.create());
+document.body.prepend(titleObject.create());
+headerObject.initStickyHeader();
